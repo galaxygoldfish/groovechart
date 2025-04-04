9fa82de553b60b2977a655db49c1e63933e754e5
galaxygoldfish <sebastianhriscu25@gmail.com>
2025-04-03 17:15:33 -0700

    [Android] Moving to single-activity architecture to implement navigation host/controller

--- android/src/main/java/com/groovechart/app/android/activity/MainActivity.kt
+++ android/src/main/java/com/groovechart/app/android/activity/MainActivity.kt
@@ -6,20 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.groovechart.app.android.GroovechartTheme
+import com.groovechart.app.android.view.OnboardingView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GroovechartTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
-            }
+            WindowCompat
+                .getInsetsController(window, window.decorView)
+                .isAppearanceLightStatusBars = true
+            // to be updated with nav controller
+            OnboardingView()
         }
     }
 }
